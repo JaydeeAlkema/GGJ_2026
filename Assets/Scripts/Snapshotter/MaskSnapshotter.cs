0d8e64a723c18aa9126308957c6a4e09a9340a22
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2026-01-31 00:44:18 +0100

    feat: add mask snapshotter

---
+++ Assets/Scripts/Snapshotter/MaskSnapshotter.cs
@@ -0,0 +1,37 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Snapshotter
+{
+	public class MaskSnapshotter : MonoBehaviour
+	{
+		[BoxGroup("References")]
+		[SerializeField] private Camera CaptureCamera;
+
+		private Sprite _snapshotSprite;
+
+		[Button]
+		public void Snapshot()
+		{
+			CaptureCamera.Render();
+
+			RenderTexture rt = CaptureCamera.targetTexture;
+			RenderTexture.active = rt;
+
+			Texture2D tex = new(rt.width, rt.height, TextureFormat.ARGB32, false);
+			tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+			tex.Apply();
+
+			RenderTexture.active = null;
+
+			Sprite snapshotSprite = Sprite.Create(
+				tex,
+				new Rect(0, 0, tex.width, tex.height),
+				new Vector2(0.5f, 0.5f),
+				100f
+			);
+
+			_snapshotSprite = snapshotSprite;
+		}
+	}
+}
