975a4e4d9d196f7fb5370e25c28f3f921ae65f70
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2026-01-30 20:06:45 +0100

    chore: add mask component

---
+++ Assets/Scripts/IMaskComponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public interface IMaskComponent
+{
+	void SetVisuals(Sprite sprite);
+	Sprite GetVisuals();
+
+	bool GetIsLocked();
+	void Lock();
+	void Unlock();
+
+	void Drag();
+	void Drop();
+}
